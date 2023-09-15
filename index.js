6da2aa84e7c121f9a3aa2921984335e71483b30b
shekinah <nyamburamary1212@gmail.com>
2023-09-15 17:35:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  let left = dodger.style.left;
+  left = parseInt(left) || 0; // Parsing to integer with a fallback to 0
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = dodger.style.left;
+  left = parseInt(left) || 0; // Parsing to integer with a fallback to 0
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+  
